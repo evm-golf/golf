5dfbf5cca82ebdf6cb74d977724623905ae4b8fd
Your Name <you@example.com>
2022-09-02 09:39:22 +0000

    dev

--- contracts/Solution.sol
+++ contracts/Solution.sol
@@ -3,12 +3,7 @@ pragma solidity ^0.8.16;
 
 contract Solution {
     fallback(bytes calldata I) external returns (bytes memory O) {
-        (uint x) =  abi.decode(I, (uint));
-        for (uint y = 0; y <= x; y++) {
-            if ((y*y <= x) &&  (x < (y+1)*(y+1))) {
-                O = abi.encode(y);
-                break;
-            }
-        }
+        (uint x, uint y) =  abi.decode(I, (uint, uint));
+        O = abi.encode(x+y);
     }
 }
