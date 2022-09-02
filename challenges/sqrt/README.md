a78015d7619dd42a058eedaa9fc17a2003a2f6de
Your Name <you@example.com>
2022-09-02 09:31:27 +0000

    dev

---
+++ challenges/sqrt/README.md
@@ -0,0 +1,18 @@
+# Addition
+
+calulate `sqrt(x)`
+
+## input
+
+- `uint256` `x`: square integer between `[0, 2**256)`
+
+## output
+
+- `uint256` `y`: `y * y = x`
+
+## example
+
+- `x = 1`, `y = 1`
+- `x = 16`, `y = 4`
+
+[sample soluton](Solution.sol)
---
+++ challenges/sqrt/Solution.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.16;
+
+contract Solution {
+    fallback(bytes calldata I) external returns (bytes memory O) {
+        (uint x) =  abi.decode(I, (uint));
+        for (uint y = 0; y <= x; y++) {
+            if (y * y == x) {
+                O = abi.encode(y);
+                break;
+            }
+        }
+    }
+}
