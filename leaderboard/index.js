6ac2f417b443faf09294478dd8f47f0103866a0b
Your Name <you@example.com>
2022-09-02 23:19:30 +0000

    dev

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -1,5 +1,10 @@
 (async function () {
     const data = await fetch('https://evm-golf.github.io/statistics/index.json');
-    const json = data.json();
-    console.log(json)
-  })();
+    const json = await data.json();
+    const table = document.getElementById('data');
+    for (const [challenge, { gas, length }] of Object.entries(json)) {
+        console.log(challenge);
+        console.log(gas);
+        console.log(length);
+    }
+})();
