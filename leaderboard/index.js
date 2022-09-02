25c58d27924da9556bfa8f7bcb01a19041342428
Your Name <you@example.com>
2022-09-02 23:25:52 +0000

    dev

--- leaderboard/index.js
+++ leaderboard/index.js
@@ -3,8 +3,19 @@
     const json = await data.json();
     const table = document.getElementById('data');
     for (const [challenge, { gas, length }] of Object.entries(json)) {
-        console.log(challenge);
-        console.log(gas);
-        console.log(length);
+        const tr = document.createElement('tr');
+        const tc = document.createElement('td');
+        const tg = document.createElement('td');
+        const tl = document.createElement('td');
+        const tgc = document.createElement('code');
+        const tlc = document.createElement('code');
+        tc.innerText = challenge;
+        tgc.innerText = `${gas.gas} (@${gas.user})`;
+        tlc.innerText = `${length.length} (@${length.user})`;
+        tg.appendChild(tgc);
+        tl.appendChild(tlc);
+        tr.appendChild(tc);
+        tr.appendChild(tg);
+        tr.appendChild(tl);
     }
 })();
