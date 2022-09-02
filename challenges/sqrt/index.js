ab75a13308fb8bc4e1b4dbc4d35f4b35a17ec23c
Your Name <you@example.com>
2022-09-02 09:46:22 +0000

    dev

--- challenges/sqrt/index.js
+++ challenges/sqrt/index.js
@@ -9,18 +9,36 @@ module.exports = [
     { x: ethers.utils.defaultAbiCoder.encode(['uint'], [5]), y: ethers.utils.defaultAbiCoder.encode(['uint'], [2]) },
     { x: ethers.utils.defaultAbiCoder.encode(['uint'], [6]), y: ethers.utils.defaultAbiCoder.encode(['uint'], [2]) },
     { x: ethers.utils.defaultAbiCoder.encode(['uint'], [7]), y: ethers.utils.defaultAbiCoder.encode(['uint'], [2]) },
-    // ...Array.from(Array(15), (_, i) => {
-    //     const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(i + 1));
-    //     const x0 = y0.mul(y0);
-    //     const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x0]);
-    //     const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
-    //     return { x: x, y: y };
-    // }),
-    // ...Array.from(Array(16), (_, i) => {
-    //     const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(16));
-    //     const x0 = y0.mul(y0);
-    //     const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x0]);
-    //     const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
-    //     return { x: x, y: y };
-    // }),
+    ...Array.from(Array(15), (_, i) => {
+        const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(i + 1));
+        const x0 = y0.mul(y0);
+        const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x0]);
+        const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
+        return { x: x, y: y };
+    }),
+    ...Array.from(Array(15), (_, i) => {
+        const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(i + 1));
+        const x0 = y0.mul(y0);
+        const x1 = x0.add(ethers.BigNumber.from(ethers.utils.randomBytes(i + 1)).mod(y0));
+        const x2 = x1.add(ethers.BigNumber.from(ethers.utils.randomBytes(i + 1)).mod(y0));
+        const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x2]);
+        const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
+        return { x: x, y: y };
+    }),
+    ...Array.from(Array(8), (_, i) => {
+        const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(16));
+        const x0 = y0.mul(y0);
+        const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x0]);
+        const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
+        return { x: x, y: y };
+    }),
+    ...Array.from(Array(8), (_, i) => {
+        const y0 = ethers.BigNumber.from(ethers.utils.randomBytes(16));
+        const x0 = y0.mul(y0);
+        const x1 = x0.add(ethers.BigNumber.from(ethers.utils.randomBytes(16)).mod(y0));
+        const x2 = x1.add(ethers.BigNumber.from(ethers.utils.randomBytes(16)).mod(y0));
+        const x = ethers.utils.defaultAbiCoder.encode(['uint'], [x2]);
+        const y = ethers.utils.defaultAbiCoder.encode(['uint'], [y0]);
+        return { x: x, y: y };
+    }),
 ];
