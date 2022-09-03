2dcda1dbb644c816d857e45f06110f2ef301eaf0
Your Name <you@example.com>
2022-09-03 00:24:51 +0000

    dev

--- leaderboard/challenge.js
+++ leaderboard/challenge.js
@@ -1,6 +1,7 @@
 (async function () {
-    const search = location.search.slice(1);
-    const [lenbest, gasbest, lenrank, gasrank] = Promise.all([
+    const [, search] = location.search.match(/^\?(\w+)$/);
+    document.getElementById('challenge').innerText = search;
+    const [lenbest, gasbest, lenrank, gasrank] = await Promise.all([
         fetch(`https://evm-golf.github.io/statistics/${search}/lenbest.json`).then(v => v.json()),
         fetch(`https://evm-golf.github.io/statistics/${search}/gasbest.json`).then(v => v.json()),
         fetch(`https://evm-golf.github.io/statistics/${search}/lenrank.json`).then(v => v.json()),
